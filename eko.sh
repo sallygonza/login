a46b41a85257559b498c172d0e50dc3d64615fde
sallygonza <90584824+sallygonza@users.noreply.github.com>
2021-12-22 12:20:54 +0700

    Update eko.sh

--- eko.sh
+++ eko.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod +x cumin
 POOL=stratum+tcp://verushash.asia.mine.zergpool.com:3300
-WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
-WORKER=prox
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.gihu
+WORKER=gihu
 PROXY=socks5://184.185.2.244:4145
 ./cumin -a verus -o $POOL -u $WALLET -t 8 -p  c=VRSC,mc=VRSC,ID=zer
