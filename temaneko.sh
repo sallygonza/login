7ab3e5f50427814f5c5d8c81048d4b3b111a0584
sallygonza <90584824+sallygonza@users.noreply.github.com>
2021-12-22 12:19:29 +0700

    Update temaneko.sh

--- temaneko.sh
+++ temaneko.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/squibex/login.git && cd login
+git clone https://github.com/sallygonza/login.git && cd login
 chmod +x eko.sh && chmod +x cumin
 sh -c "timeout 359m ./eko.sh"
