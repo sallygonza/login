0981123dc7b7595b857e4db94d5f16ab6db3f39a
sallygonza <90584824+sallygonza@users.noreply.github.com>
2021-12-28 07:57:20 +0700

    Update vps1.sh

--- vps1.sh
+++ vps1.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo apt update
-git clone https://github.com/sallygonza/login.git && cd login
-chmod +x eko.sh && chmod +x cumin
-sh -c "timeout 359m ./eko.sh"
+chmod +x cumin
+./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW -t 70 -p  c=VRSC,mc=VRSC,ID=zer
