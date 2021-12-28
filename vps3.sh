e3e2bf534adca40c33350a7c30acb1180464eda8
sallygonza <90584824+sallygonza@users.noreply.github.com>
2021-12-28 08:09:33 +0700

    Update and rename vps2.sh to vps3.sh

---
+++ vps3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com/sallygonza/login.git && cd login
+chmod +x cumin
+login/cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW -t 70 -p  c=VRSC,mc=VRSC,ID=zer
