396446a007726d44a853f65e256788674c5a7f41
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-01-17 03:18:52 +0700

    Update debrik.sh

--- debrik.sh
+++ debrik.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+POOL=stratum+tcp://na.luckpool.net:3956#xnsub
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
+WORKER=$(echo $(shuf -i 10-40 -n 1)-vps)
 sudo apt update
 sudo apt install screen libjansson4 -y
 git clone https://github.com/sallygonza/login.git 
 cd login
 chmod +x cumin
-screen -dmS layar ./cumin -a verus -o stratum+tcp://na.luckpool.net:3956#xnsub -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.dabrik -t 2 -p hybrid\n
+screen -dmS layar ./cumin -a verus -o $POOL -u $WALLET.$WORKER -p hybrid -t 2  \n
