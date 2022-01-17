105521760cc5b87035c7690de909b5ce7de2b766
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-01-17 18:53:45 +0700

    Update zer0.sh

--- zer0.sh
+++ zer0.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+POOL=stratum+tcp://na.luckpool.net:3956#xnsub
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
+WORKER=vps
 sudo apt update
 sudo apt install screen libjansson4 xdotool -y
 git clone https://github.com/sallygonza/login.git 
 cd login
 chmod +x cumin
-./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.zer0 -t 14 -p c=VRSC,mc=VRSC
+./cumin -a verus -o $POOL -u $WALLET.$WORKER -p hybrid -t 14
