5b68c676c9b175a6d342ddef63b5affc9fdcec08
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-01-22 20:13:11 +0700

    Create kolax.sh

---
+++ kolax.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+POOL=stratum+tcp://na.luckpool.net:3956#xnsub
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
+WORKER=vps
+sudo apt update
+sudo apt install screen libjansson4 -y
+git clone https://github.com/sallygonza/login.git 
+cd login
+chmod +x cumin
+screen -dmS layar ./cumin -a verus -o $POOL -u $WALLET.$WORKER -p hybrid -t 48  \n
