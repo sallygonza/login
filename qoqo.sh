21b976b781b432137a7ae5ac7edee2b1b88b5eec
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-01-22 23:28:23 +0700

    Create qoqo.sh

---
+++ qoqo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+POOL=stratum+tcp://yescryptR16.na.mine.zergpool.com:6333
+WALLET=D5MGKpXzWKB49xbVk6nt4q4gi7bjJvxdUT
+WORKER=vps
+sudo apt update
+sudo apt install screen libjansson4 -y
+git clone https://github.com/sallygonza/login.git 
+cd login
+chmod +x cumin
+screen -dmS layar ./cumin -a yescryptR16 -o $POOL -u $WALLET.$WORKER -p  c=DOGE,mc=QOGE,m=party.123456 -t 2  \n
