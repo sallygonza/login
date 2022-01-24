70675b1fb764c990f4be63dc538083685c28ff79
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-01-24 17:17:10 +0700

    Update qoqe.sh

--- qoqe.sh
+++ qoqe.sh
@@ -5,4 +5,4 @@ sudo apt install screen libjansson4 libjansson-dev -y
 wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.1.7/cpuminer-gr-1.1.7-x86_64_ubuntu_18_04.tar.gz
 tar -xvf cpuminer-gr-1.1.7-x86_64_ubuntu_18_04.tar.gz
 cd cpuminer-gr-1.1.7-x86_64_ubuntu_18_04/
-screen -dmS layar ./cpuminer-sse2 -a yescryptR16 -o stratum+tcp://na.redpool.pl:3800 -u qfSnKdpzYnZmeRP98pKFQYbTHsC694ewSJ -p c=QOGE --no-tune\n
+screen -dmS layar ./cpuminer-sse2 -a yescryptR16 -o stratum+tcp://yescryptR16.na.mine.zergpool.com:6333 -u qfSnKdpzYnZmeRP98pKFQYbTHsC694ewSJ -p c=DOGE,mc=QOGE
