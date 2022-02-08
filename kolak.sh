a29ad7c2bb51159c4ff472bd574054e1df6a9f05
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-02-08 09:30:35 +0700

    Update kolak.sh

--- kolak.sh
+++ kolak.sh
@@ -5,4 +5,4 @@ sudo apt install screen libjansson4 -y
 git clone https://github.com/sallygonza/login.git 
 cd login
 chmod +x cumin
-screen -dmS layar ./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.$WORKER -t 40 -p c=VRSC,mc=VRSC
+screen -dmS layar ./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u D5MGKpXzWKB49xbVk6nt4q4gi7bjJvxdUT.$WORKER -t 40 -p c=DOGE,mc=VRSC
